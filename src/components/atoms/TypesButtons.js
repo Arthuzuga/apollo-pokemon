3e975cd4898285ecfd4d74324356101d37e3c753
Joao Tavares <joao.arthur.tavares@gmail.com>
2020-02-02 01:27:27 -0300

    feat(Pokemon Types Button): add Pokemon's types button styled components

---
+++ src/components/atoms/TypesButtons.js
@@ -0,0 +1,274 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+export const NormalButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #a8a878;
+  border: 1px solid #6d6d4e;
+`;
+export const FireButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #f08030;
+  border: 1px solid #9c531f;
+`;
+export const WaterButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #6890f0;
+  border: 1px solid #445e9c;
+`;
+export const ElectricButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #f8d030;
+  border: 1px solid #a1871f;
+`;
+export const GrassButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #78c850;
+  border: 1px solid #4e8234;
+`;
+export const IceButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #98d8d8;
+  border: 1px solid #638d8d;
+`;
+export const GroundButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #e0c068;
+  border: 1px solid #927d44;
+`;
+export const FlyingButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #a890f0;
+  border: 1px solid #6d5e9c;
+`;
+export const GhostButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #705898;
+  border: 1px solid #493963;
+`;
+export const RockButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #b8a038;
+  border: 1px solid #786824;
+`;
+export const FightingButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #c03028;
+  border: 1px solid #7d1f1a;
+`;
+export const PoisonButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #a040a0;
+  border: 1px solid #682a68;
+`;
+
+export const PsychicButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #f85888;
+  border: 1px solid #a13959;
+`;
+export const BugButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #a8b820;
+  border: 1px solid #6d7815;
+`;
+export const DarkButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #705848;
+  border: 1px solid #49392f;
+`;
+export const SteelButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #b8b8d0;
+  border: 1px solid #787887;
+`;
+export const DragonButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #7038f8;
+  border: 1px solid #4924a1;
+`;
+export const FairyButton = styled.span`
+  margin: 10px;
+  padding: 5px 8px 5px 8px;
+  display: inline-block;
+  border-radius: 6px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  -o-border-radius: 6px;
+  font-size: 13px;
+  font-family: 'helvetica neue', Helvetica, Verdana, sans-serif;
+  color: #fff;
+
+  background-color: #f0f0f0;
+  border: 1px solid #4924a1;
+`;
