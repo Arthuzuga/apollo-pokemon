4043edc77fb6041fb8f0e7ffb45075b178a070c6
Joao Tavares <joao.arthur.tavares@gmail.com>
2020-02-06 00:25:42 -0300

    fix: a lot of thing, sorry it's late and i'm tired

--- src/apollo/clients/index.js
+++ src/apollo/clients/index.js
@@ -1,22 +1,7 @@
 import ApolloClient from 'apollo-boost';
 
-const defaultOptions = {
-  watchQuery: {
-    fetchPolicy: 'cache-first',
-    errorPolicy: 'ignore'
-  },
-  query: {
-    fetchPolicy: 'cache-first',
-    errorPolicy: 'all'
-  },
-  mutate: {
-    errorPolicy: 'all'
-  }
-};
-
 const client = new ApolloClient({
-  uri: 'https://graphql-pokemon.now.sh/',
-  defaultOptions: defaultOptions
+  uri: 'https://graphql-pokemon.now.sh/'
 });
 
 export default client;
