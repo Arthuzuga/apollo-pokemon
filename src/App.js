df9dbfff7f1540d99d4ed02255c36cd364e89fee
Joao Tavares <joao.arthur.tavares@gmail.com>
2020-02-11 21:53:45 -0300

    refact(Templates and States): Add templates to change complex components and add new States

--- src/App.js
+++ src/App.js
@@ -3,31 +3,13 @@ import { useQuery, useLazyQuery } from '@apollo/react-hooks';
 import styled from 'styled-components';
 
 import { Progress } from 'antd';
-import 'antd/dist/antd.css';
 
 import Pokedex from './components/templates/Pokedex';
-import {
-  NormalButton,
-  FireButton,
-  WaterButton,
-  ElectricButton,
-  GrassButton,
-  IceButton,
-  GroundButton,
-  FlyingButton,
-  GhostButton,
-  RockButton,
-  FightingButton,
-  PoisonButton,
-  PsychicButton,
-  BugButton,
-  DarkButton,
-  SteelButton,
-  DragonButton,
-  FairyButton
-} from './components/atoms/TypesButtons';
+import PokemonBox from './components/templates/PokemonBox';
+
 import { GET_POKEMONS } from './apollo/queries/getPokemons';
 import { GET_POKEMON } from './apollo/queries/getPokemon';
+import PokemonInfo from './components/templates/PokemonInfo.js';
 
 const Container = styled.div`
   width: 100%;
@@ -40,69 +22,19 @@ const Container = styled.div`
 `;
 
 const PokemonList = styled.div`
-  width: 80%;
+  width: 100%;
   margin-top: 2rem;
   display: flex;
   justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  flex-wrap: wrap;
+  align-items: flex-start;
+  flex-direction: row;
 `;
-
-const PokemonRow = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const pokemonTypes = type => {
-  switch (type) {
-    case 'Normal':
-      return <NormalButton>{type}</NormalButton>;
-    case 'Fire':
-      return <FireButton>{type}</FireButton>;
-    case 'Water':
-      return <WaterButton>{type}</WaterButton>;
-    case 'Electric':
-      return <ElectricButton>{type}</ElectricButton>;
-    case 'Grass':
-      return <GrassButton>{type}</GrassButton>;
-    case 'Ice':
-      return <IceButton>{type}</IceButton>;
-    case 'Ground':
-      return <GroundButton>{type}</GroundButton>;
-    case 'Flying':
-      return <FlyingButton>{type}</FlyingButton>;
-    case 'Ghost':
-      return <GhostButton>{type}</GhostButton>;
-    case 'Rock':
-      return <RockButton>{type}</RockButton>;
-    case 'Fighting':
-      return <FightingButton>{type}</FightingButton>;
-    case 'Poison':
-      return <PoisonButton>{type}</PoisonButton>;
-    case 'Psychic':
-      return <PsychicButton>{type}</PsychicButton>;
-    case 'Bug':
-      return <BugButton>{type}</BugButton>;
-    case 'Dark':
-      return <DarkButton>{type}</DarkButton>;
-    case 'Steel':
-      return <SteelButton>{type}</SteelButton>;
-    case 'Dragon':
-      return <DragonButton>{type}</DragonButton>;
-    case 'Fairy':
-      return <DragonButton>{type}</DragonButton>;
-    default:
-      return <span>{type}</span>;
-  }
-};
 
 export const App = () => {
   const [index, setIndex] = useState(0);
   const [progress, setProgress] = useState(0);
   const [pokemonCatched, savePokemons] = useState([]);
+  const [pokemonSelected, selectPokemon] = useState({});
 
   const { data, loading, networkStatus } = useQuery(GET_POKEMONS, {
     notifyOnNetworkStatusChange: true
@@ -113,23 +45,18 @@ export const App = () => {
       notifyOnNetworkStatusChange: true,
       onCompleted: () => {
         const { pokemon } = pokemonData;
-        savePokemons([...pokemonCatched, { ...pokemon, listId: (Math.random() * 100).toFixed(0) }]);
+        selectPokemon(pokemon);
       }
     }
   );
 
-  const getPokemonToList = (id, name) => {
-    getPokemonInfo({
-      variables: {
-        id,
-        name
-      }
-    });
+  const getPokemonToList = pokemon => {
+    savePokemons([...pokemonCatched, { ...pokemon, listId: (Math.random() * 100).toFixed(0) }]);
   };
 
   useEffect(() => {
     setProgress(Math.round(100 / networkStatus));
-  }, [networkStatus, pokemonCatched]);
+  }, [networkStatus, pokemonCatched, pokemonSelected, pokemonLoading]);
 
   if (loading) {
     return (
@@ -168,40 +95,12 @@ export const App = () => {
           classification: pokemons[index].classification
         }}
         pokemonCatch={() => {
-          getPokemonToList(pokemons[index].id, pokemons[index].name);
+          getPokemonToList(pokemons[index]);
         }}
       />
       <PokemonList>
-        {pokemonLoading ? (
-          <div style={{ padding: '1rem' }}>
-            <span>Carregando...</span>
-          </div>
-        ) : pokemonCatched.length > 0 ? (
-          pokemonCatched.map(({ name, types, listId }, index) => (
-            <PokemonRow key={index}>
-              <span>{name}</span>
-              <div>
-                <div>
-                  {types.map(type => {
-                    return <React.Fragment key={type}>{pokemonTypes(type)}</React.Fragment>;
-                  })}
-                </div>
-                <div
-                  style={{ cursor: 'pointer' }}
-                  onClick={event => {
-                    savePokemons(pokemonCatched.filter(pokemon => pokemon.listId !== listId));
-                  }}
-                >
-                  deletar
-                </div>
-              </div>
-            </PokemonRow>
-          ))
-        ) : (
-          <div style={{ padding: '1rem' }}>
-            <span>Lista Vazia, Vamos pegar alguns Pokémons!</span>
-          </div>
-        )}
+        <PokemonInfo loading={pokemonLoading} pokemon={pokemonSelected} />
+        <PokemonBox pokemons={pokemonCatched} selectPokemon={getPokemonInfo} />
       </PokemonList>
     </Container>
   );
